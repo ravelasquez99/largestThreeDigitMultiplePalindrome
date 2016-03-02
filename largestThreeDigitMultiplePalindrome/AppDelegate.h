e65c9a9934a5889cc9ed2382784302fc93bb35fc
ravelasquez99 <ricky.wedew@gmail.com>
2016-03-02 14:08:51 -0800

    Initial Commit

---
+++ largestThreeDigitMultiplePalindrome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  largestThreeDigitMultiplePalindrome
+//
+//  Created by Richard Velazquez on 3/2/16.
+//  Copyright © 2016 Richard Velazquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ largestThreeDigitMultiplePalindrome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  largestThreeDigitMultiplePalindrome
+//
+//  Created by Richard Velazquez on 3/2/16.
+//  Copyright © 2016 Richard Velazquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
